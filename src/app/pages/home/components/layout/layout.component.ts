0ed628b93feb3af97ddbe53b5a4790443a497fbd
Christian Torres <cmtorres@minsait.com>
2025-03-29 13:26:15 -0600

    update - inicio layout (background 100%)

--- src/app/pages/home/components/layout/layout.component.ts
+++ src/app/pages/home/components/layout/layout.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { BackgroundImagePipe } from '../../../../shared/pipes/backgound-images.pipe';
 
 @Component({
   selector: 'home-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    BackgroundImagePipe
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
+
 export class LayoutComponent {
+
+  public urlImageBackground: string = 'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2';
 
 }
