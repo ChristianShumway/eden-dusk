f1cc6e79cbd898bcde68dc7f31bf1a6ac7dfd7f2
Christian Torres <cmtorres@minsait.com>
2025-04-11 14:38:51 -0600

    update - estilos footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { MenuItemModel } from '../../../core/models/item-menu.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'shared-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  public urlimageWhite = 'https://raw.githubusercontent.com/ChristianShumway/eden-dusk/refs/heads/master/src/assets/images/logos/EdenDusk-white.png';
+  private readonly fullDate = new Date();
+  public year = this.fullDate.getFullYear()
+  public itemsMenu: MenuItemModel[] = [
+    { name: 'home', path: '/home' },
+    { name: 'transmisiones', path: '/transmisiones' },
+    { name: 'nuestra historia', path: '/historia' },
+    { name: 'blog', path: '/blog' },
+    { name: 'galeria', path: '/galeria' }
+  ];
+
 
 }
