344ad0d112e44b6cc3b187f16f704a2a1b91dcf1
Christian Torres <cmtorres@minsait.com>
2025-04-23 20:48:58 -0600

    update - implementación de endpoints transmisiones en el home

--- src/app/pages/home/components/eventos-transmisiones/eventos-transmisiones.component.ts
+++ src/app/pages/home/components/eventos-transmisiones/eventos-transmisiones.component.ts
@@ -1,17 +1,31 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { TransmisionModel } from '../../../../core/models/transmission.model';
+import { NoEventosComponent } from '../../../../shared/components/no-eventos/no-eventos.component';
+import { RouterModule } from '@angular/router';
+import { SvgService } from '../../../../core/services/svg.service';
+import { SafeHtml } from '@angular/platform-browser';
+import { SvgIcons } from '../../../../core/utils/svg-icons.enum';
 
 @Component({
   selector: 'home-eventos-transmisiones',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    NoEventosComponent
   ],
   templateUrl: './eventos-transmisiones.component.html',
   styleUrl: './eventos-transmisiones.component.scss'
 })
 export class EventosTransmisionesComponent {
 
-  fechaHoy = new Date;
+  private readonly svgService = inject(SvgService);
+
+  public proximoEventos = input.required<TransmisionModel[]>();
+  public eventosPasados = input.required<TransmisionModel[]>();
+
+  public svgCalendar = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.calendar));
+  public svgCamera = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.camera));
 
 }
--- src/app/pages/home/page/main-home/main-home.component.ts
+++ src/app/pages/home/page/main-home/main-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { LayoutComponent } from '../../components/layout/layout.component';
 import { QuienEsEdenDuskComponent } from '../../components/quien-es-eden-dusk/quien-es-eden-dusk.component';
 import { PreviewPortfolioComponent } from '../../components/preview-portfolio/preview-portfolio.component';
@@ -6,6 +6,8 @@ import { PreviewStoreComponent } from '../../components/preview-store/preview-st
 import { NovedadesComponent } from '../../components/novedades/novedades.component';
 import { NewsLetterComponent } from '../../components/news-letter/news-letter.component';
 import { EventosTransmisionesComponent } from '../../components/eventos-transmisiones/eventos-transmisiones.component';
+import { TransmisionModel } from '../../../../core/models/transmission.model';
+import { TransmisionesService } from '../../../../core/services/transmisiones.service';
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -21,6 +23,36 @@ import { EventosTransmisionesComponent } from '../../components/eventos-transmis
   templateUrl: './main-home.component.html',
   styleUrl: './main-home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  private readonly transmisionesService = inject(TransmisionesService);
+
+  public eventos = signal<TransmisionModel[]>([]);
+  public eventosPasados = signal<TransmisionModel[]>([]);
+  public dateToSearh = new Date();
+
+  ngOnInit(): void {
+    this.getTransmissionsByMonth(this.dateToSearh);
+    this.getLastTransmissions();
+  }
+
+
+  getTransmissionsByMonth(date: Date) {
+    this.transmisionesService.getTransmissionsByMonth(date).subscribe({
+      next: response => {
+        this.eventos.set([...response]);
+      },
+      error: error => console.error(error)
+    })
+  }
+
+  getLastTransmissions() {
+    this.transmisionesService.getLastTransmissions().subscribe({
+      next: response => {
+        this.eventosPasados.set([...response]);
+      },
+      error: error => console.error(error)
+    })
+  }
 
 }
