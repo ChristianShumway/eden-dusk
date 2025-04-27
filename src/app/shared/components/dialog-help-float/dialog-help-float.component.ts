dad767f97618786449699ed6b2dbcbaf98e1c0b3
Christian Torres <cmtorres@minsait.com>
2025-04-26 21:09:47 -0600

    update diseño parte dark

--- src/app/shared/components/dialog-help-float/dialog-help-float.component.ts
+++ src/app/shared/components/dialog-help-float/dialog-help-float.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, input, signal } from '@angular/core';
+import { AfterViewInit, Component, inject, input, signal } from '@angular/core';
 import { SvgService } from '../../../core/services/svg.service';
 import { SafeHtml } from '@angular/platform-browser';
 import { SvgIcons } from '../../../core/utils/svg-icons.enum';
+import { initPopovers } from 'flowbite';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'shared-dialog-help-float',
@@ -10,12 +12,24 @@ import { SvgIcons } from '../../../core/utils/svg-icons.enum';
   templateUrl: './dialog-help-float.component.html',
   styleUrl: './dialog-help-float.component.scss'
 })
-export class DialogHelpFloatComponent {
+export class DialogHelpFloatComponent implements AfterViewInit {
 
+  private readonly router = inject(Router);
   private readonly svgService = inject(SvgService);
+
 
   public titleMsg = input.required<string>();
   public descriptionMsg = input.required<string>();
   public svgHelp = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.userHelp))
+
+
+  ngAfterViewInit(): void {
+    initPopovers();
+    this.router.events.subscribe(() => {
+      setTimeout(() => {
+        initPopovers(); // vuelve a correrlo cuando cambia la ruta
+      }, 100); // pequeño delay para que el DOM esté listo
+    });
+  }
 
 }
