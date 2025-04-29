a650d3c18a9ab2ec1119cd32665643de581fce47
Christian Torres <cmtorres@minsait.com>
2025-04-29 17:09:43 -0600

    update - inicio maquetación feed articulos

--- src/app/pages/blog/components/articulo-feed/articulo-feed.component.ts
+++ src/app/pages/blog/components/articulo-feed/articulo-feed.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { BackgroundImagePipe } from '../../../../shared/pipes/backgound-images.pipe';
+import { PathsEnum } from '../../../../core/utils/paths.enum';
 
 @Component({
-  selector: 'app-articulo-feed',
+  selector: 'blog-articulo-feed',
   standalone: true,
-  imports: [],
+  imports: [
+    BackgroundImagePipe
+  ],
   templateUrl: './articulo-feed.component.html',
   styleUrl: './articulo-feed.component.scss'
 })
 export class ArticuloFeedComponent {
+
+  public urlImageBackground: string = `${PathsEnum.URLIMAGES}/_NEL2385-min.jpg`;
 
 }
--- src/app/pages/blog/components/feed-articulos/feed-articulos.component.ts
+++ src/app/pages/blog/components/feed-articulos/feed-articulos.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ArticuloFeedComponent } from '../articulo-feed/articulo-feed.component';
+import { SvgService } from '../../../../core/services/svg.service';
+import { SafeHtml } from '@angular/platform-browser';
+import { SvgIcons } from '../../../../core/utils/svg-icons.enum';
 
 @Component({
   selector: 'blog-feed-articulos',
   standalone: true,
-  imports: [],
+  imports: [
+    ArticuloFeedComponent
+  ],
   templateUrl: './feed-articulos.component.html',
   styleUrl: './feed-articulos.component.scss'
 })
 export class FeedArticulosComponent {
+
+  private readonly svgService = inject(SvgService);
+
+  public svgAngle = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.angleRight));
 
 }
