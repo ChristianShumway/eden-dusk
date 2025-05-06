2fcc013af89dcaff0220e54ccd7603a89240380e
Christian Torres <cmtorres@minsait.com>
2025-05-06 13:50:11 -0600

    fix boton desplegable compartir en redes sociales al no recargar pagina

--- src/app/shared/components/shared-socialmedia/shared-socialmedia.component.ts
+++ src/app/shared/components/shared-socialmedia/shared-socialmedia.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, signal } from '@angular/core';
-import { SvgService } from '../../../core/services/svg.service';
+import { Component, inject, signal, AfterViewInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { initDials, initTooltips } from 'flowbite';
 import { SafeHtml } from '@angular/platform-browser';
+import { SvgService } from '../../../core/services/svg.service';
 import { SvgIcons } from '../../../core/utils/svg-icons.enum';
-
 
 @Component({
   selector: 'shared-socialmedia',
@@ -11,7 +12,10 @@ import { SvgIcons } from '../../../core/utils/svg-icons.enum';
   templateUrl: './shared-socialmedia.component.html',
   styleUrl: './shared-socialmedia.component.scss'
 })
-export class SharedSocialmediaComponent {
+export class SharedSocialmediaComponent implements AfterViewInit {
+
+    private readonly router = inject(Router);
+
 
   private readonly svgService = inject(SvgService);
   public iconShared = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.shared));
@@ -19,7 +23,15 @@ export class SharedSocialmediaComponent {
   public iconFacebook = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.facebook));
   public iconX = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.twitter));
 
-
-
+  ngAfterViewInit(): void {
+    initDials();
+    initTooltips();
+    this.router.events.subscribe(() => {
+      setTimeout(() => {
+        initDials(); // vuelve a correrlo cuando cambia la ruta
+        initTooltips();
+      }, 100); // pequeño delay para que el DOM esté listo
+    });
+  }
 
 }
