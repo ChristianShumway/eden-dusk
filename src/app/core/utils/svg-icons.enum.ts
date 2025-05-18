03f1e625020706d5a5f88a16ce114e5af2f52031
Christian Torres <cmtorres@minsait.com>
2025-05-18 13:12:07 -0600

    inicio diseño colección de galerias

--- src/app/core/utils/svg-icons.enum.ts
+++ src/app/core/utils/svg-icons.enum.ts
@@ -100,5 +100,11 @@ export enum SvgIcons {
     <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
     </svg>
+  `,
+  'mapLocation' = `
+    <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+      <path fill-rule="evenodd" d="M11.906 1.994a8.002 8.002 0 0 1 8.09 8.421 7.996 7.996 0 0 1-1.297 3.957.996.996 0 0 1-.133.204l-.108.129c-.178.243-.37.477-.573.699l-5.112 6.224a1 1 0 0 1-1.545 0L5.982 15.26l-.002-.002a18.146 18.146 0 0 1-.309-.38l-.133-.163a.999.999 0 0 1-.13-.202 7.995 7.995 0 0 1 6.498-12.518ZM15 9.997a3 3 0 1 1-5.999 0 3 3 0 0 1 5.999 0Z" clip-rule="evenodd"/>
+    </svg>
+
   `
 }
--- src/app/pages/galeria/components/coleccion/coleccion.component.ts
+++ src/app/pages/galeria/components/coleccion/coleccion.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { SvgService } from '../../../../core/services/svg.service';
+import { SafeHtml } from '@angular/platform-browser';
+import { SvgIcons } from '../../../../core/utils/svg-icons.enum';
 
 @Component({
   selector: 'gallery-coleccion',
@@ -9,4 +12,8 @@ import { Component } from '@angular/core';
 })
 export class ColeccionComponent {
 
+  private readonly svgService = inject(SvgService);
+
+  public svgArrow = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.angleRight));
+  public svgLocation = signal<SafeHtml>(this.svgService.getSanitizedSvg(SvgIcons.mapLocation));
 }
